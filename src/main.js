05f725f5f2df7d3ce128733af13c483673e93dc1
Wayne Gomes <waynegomes.wg@gmail.com>
2020-07-29 14:42:50 -0300

    Routes and Preparation of the Working Space

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import './assets/reset.css';
+import './assets/style.css';
 
 Vue.config.productionTip = false;
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Lista from "../views/Lista.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Lista",
+    component: Lista
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/cadastro",
+    name: "Cadastro",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/Cadastro.vue")
   }
 ];
 
