aeeae5ca35740a4b5e5cd723f98de44ae3511197
Wayne Gomes <waynegomes.wg@gmail.com>
2020-07-30 17:47:44 -0300

    Cadastro design done, functionality started

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -11,7 +11,7 @@
     </div>
     <div class="fullwidth flexcolumn">
       <label for="cpf">Cpf</label>
-      <input id="cpf" type="text" placeholder v-model="cadastro.cpf" />
+      <input id="cpf" type="text" placeholder="111.111.111-01" v-model="cadastro.cpf" />
     </div>
     <div class="halfwidth flexcolumn">
       <label for="endereco">Endereço</label>
@@ -36,14 +36,91 @@
     </div>
     <div class="fullwidth flexcolumn hr"></div>
     <div class="fullwidth flex space-top">
-      <div class="fullwidthmobile">
-        <input type="radio" id="credito" name="opcaopagamento" value="credito" />
-        <label for="credito">Cartão de Crédito</label>
+      <div class="fullwidth">
+        <div class="fullwidthmobile">
+          <input type="radio" id="credito" name="opcaopagamento" value="credito" />
+          <label for="credito">Cartão de Crédito</label>
+        </div>
+        <div class="fullwidthmobile">
+          <input type="radio" id="boleto" name="opcaopagamento" value="boleto" />
+          <label for="boleto">Boleto Bancário</label>
+        </div>
       </div>
-      <div class="fullwidthmobile">
-        <input type="radio" id="boleto" name="opcaopagamento" value="boleto" />
-        <label for="boleto">Boleto Bancário</label>
+      <div class="halfwidth flexcolumn">
+        <label for="nome">Nome do Cartão</label>
+        <input id="nome" type="text" placeholder v-model="cadastro.cartao.nome" />
       </div>
+      <div class="halfwidthSpace"></div>
+      <div class="halfwidth flexcolumn">
+        <label for="cidade">Data de expiração</label>
+        <div class="flex">
+          <select class="halfwidthfixed" id="cidade" placeholder v-model="cadastro.cartao.datames">
+            <option value="1">Janeiro</option>
+            <option value="2">Fevereiro</option>
+            <option value="3">Março</option>
+            <option value="4">Abril</option>
+            <option value="5">Maio</option>
+            <option value="6">Junho</option>
+            <option value="7">Julho</option>
+            <option value="8">Agosto</option>
+            <option value="9">Setembro</option>
+            <option value="10">Outubro</option>
+            <option value="11">Novembro</option>
+            <option value="12">Dezembro</option>
+          </select>
+          <div class="selectArrow"></div>
+          <div class="halfwidthfixedspace"></div>
+          <select class="halfwidthfixed" id="cidade" placeholder v-model="cadastro.cartao.dataano">
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+            <option value="2028">2028</option>
+            <option value="2029">2029</option>
+            <option value="2030">2030</option>
+            <option value="2031">2031</option>
+            <option value="2032">2032</option>
+            <option value="2033">2033</option>
+            <option value="2034">2034</option>
+            <option value="2035">2035</option>
+            <option value="2036">2036</option>
+            <option value="2037">2037</option>
+            <option value="2038">2038</option>
+            <option value="2039">2039</option>
+            <option value="2040">2040</option>
+            <option value="2041">2041</option>
+            <option value="2042">2042</option>
+            <option value="2043">2043</option>
+            <option value="2044">2044</option>
+            <option value="2045">2045</option>
+            <option value="2046">2046</option>
+            <option value="2047">2047</option>
+            <option value="2048">2048</option>
+            <option value="2049">2049</option>
+            <option value="2050">2050</option>
+          </select>
+          <div class="selectArrow"></div>
+        </div>
+      </div>
+      <div class="halfwidth flexcolumn">
+        <label for="nome">Nome do Cartão</label>
+        <input id="nome" type="text" placeholder v-model="cadastro.cartao.nome" />
+      </div>
+      <div class="halfwidthSpace"></div>
+      <div class="halfwidth flexcolumn">
+        <label for="nome">Nome do Cartão</label>
+        <input id="nome" type="text" placeholder v-model="cadastro.cartao.nome" />
+      </div>
+    </div>
+    <div class="fullwidth flexcolumn hr space-top"></div>
+    <div class="fullwidth flexcolumn space-top">
+      <span class="fontw-m mtop">Seu cartão será debitado em R$ 49,00</span>
+      <button id="btmatricula" class="mtop" @click="addClass('btmatricula','animboink','250')">Realizar Matrícula</button>
+      <span class="fontw-l fonts-s mtop">Informações seguras e criptografadas</span>
     </div>
   </div>
 </template>
@@ -54,10 +131,16 @@ export default {
       cadastro: {
         nome: null,
         email: null,
-        cpf: null
+        cpf: null,
+        cartao: {}
       },
       id: null
     };
+  },
+  methods: {
+    addClass: function(){
+
+    }
   }
 };
 </script>
